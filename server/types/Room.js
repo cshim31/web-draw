e59596e3c146e463595e610b8baba4872f25ca73
Chanhuyng Shim <cshim31@gatech.edu>
2024-02-13 09:29:13 -0500

    update server side communication (Needs to be tested)

--- server/types/Room.js
+++ server/types/Room.js
@@ -3,27 +3,44 @@ const ListNode = require('./LinkedList.js');
 
 class Room {
     constructor() {
-        this.drawnData = null;
-        this.imageBase64 = [];
+        this.drawnData = {};
+        this.imageData = {};
         this.users = new LinkedList();
     }
 
     get data() {
         obj = {
             'drawnData': this.drawnData,
-            'imageBase64': this.imageBase64,
+            'imageData': this.imageData,
             'users': this.users
         }
 
         return obj;
     } 
 
-    updateDraw(data) {
-        
+    getImageID = (imageBase64) => {
+        return imageBase64.toString(16).slice(2);
     }
 
-    updateImage(data) {
-        this.imageBase64.push(data);
+
+    updateImage(imageData) {
+        let imageID = this.getImageID(imageData.imageBase64);
+        imageData[imageID] = imageData;
+    }
+
+
+    addDraw(data) {
+        data.forEach((mode) => {
+            // TODO: figure out way to set different props for each drawing
+            // Package props and coordinates (x,y) as an object
+            // Each appended to array in mode
+            drawnData[mode].append(data);
+        })
+    }
+
+    addImage(imageData) {
+        let imageID = this.getImageID(imageData.imageBase64);
+        this.imageData[imageID] = imageData;
     }
 
     addUser(userID) {
